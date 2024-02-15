6b155b380eff36ecfd117759aa22252188219b0f
Robert <remken07@gmail.com>
2024-02-15 10:43:54 -0500

    added sensor for intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.Rev2mDistanceSensor;
 
 import edu.wpi.first.math.MathUtil;
 import frc.robot.Constants.IntakeConstants;
@@ -9,6 +10,7 @@ import frc.robot.Constants.IntakeConstants;
 public class Intake {
     //initialize motor
     private final CANSparkMax intakeMotor = new CANSparkMax(IntakeConstants.kIntake, MotorType.kBrushless);
+    private final Rev2mDistanceSensor intakeSensor = new Rev2mDistanceSensor(null);
 
     public Intake() {
         intakeMotor.restoreFactoryDefaults();
@@ -16,5 +18,12 @@ public class Intake {
 
     public void setSpeed(double speed) {
         intakeMotor.set(MathUtil.clamp(speed, -1 * IntakeConstants.kMaxSpeed, IntakeConstants.kMaxSpeed));
+    }
+
+
+    public double getIntakeSensorDistance(){
+        intakeSensor.setAutomaticMode(true);
+
+        return intakeSensor.getRange();
     }
 }
