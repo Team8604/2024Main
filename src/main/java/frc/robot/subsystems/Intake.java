a237c1e6cce34af7fe8c1a45fb23abd85f8c8343
Robert <remken07@gmail.com>
2024-02-15 10:32:28 -0500

    intake and shooter subsystems

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,13 +1,20 @@
-package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
-import frc.robot.Constants;
-import frc.robot.Constants.IntakeConstants;
+package frc.robot.subsystems;  
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.math.MathUtil;
+import frc.robot.Constants.IntakeConstants;
+
 public class Intake {
     //initialize motor
     private final CANSparkMax intakeMotor = new CANSparkMax(IntakeConstants.kIntake, MotorType.kBrushless);
+
+    public Intake() {
+        intakeMotor.restoreFactoryDefaults();
+    }
+
+    public void setSpeed(double speed) {
+        intakeMotor.set(MathUtil.clamp(speed, -1 * IntakeConstants.kMaxSpeed, IntakeConstants.kMaxSpeed));
+    }
 }
