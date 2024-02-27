89b60b64ab583b1028557d90ed8717aec45ed5fe
Robert <remken07@gmail.com>
2024-02-27 18:58:17 -0500

    Update Control_Intake.java

--- src/main/java/frc/robot/commands/Control_Intake.java
+++ src/main/java/frc/robot/commands/Control_Intake.java
@@ -1,5 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
-public class Control_Intake {
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+/** An example command that uses an example subsystem. */
+public class Control_Intake extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Intake controlIntake;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public Control_Intake(Intake subsystem) {
+    controlIntake = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(controlIntake);
+  }
+
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
     
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
