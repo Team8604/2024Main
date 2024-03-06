63c30c9dfbf278bd4cb3c21d8130ed32e68f5f16
SunayAgarwal <96151207+SunayAgarwal@users.noreply.github.com>
2024-03-05 23:42:27 -0500

    Create Limelight.java

---
+++ src/main/java/frc/robot/subsystems/limelight/Limelight.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.limelight;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.limelight.LimelightHelper;
+import frc.robot.RobotContainer;
+
+public class Limelight extends SubsystemBase {
+    
+    public Limelight() {
+        
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putnumber("Limelight thing", idkrightnow);
+    }
+
+}
