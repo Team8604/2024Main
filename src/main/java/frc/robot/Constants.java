203d34e0eec9a2fe6b98cc87168b0f94ff26e249
Robert <remken07@gmail.com>
2024-03-10 16:47:35 -0400

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,13 +31,13 @@ public final class Constants {
     public static final int kLeftFollower = 4;
 
     // Track width
-    public static final int kWidth = 55;
+    public static final int kWidth = 55; //cm -- check may not be correct
 
     // Speed modifier
     public static final double kMaxSpeed = 0.7;
     public static final double kSpeedIncrease = 0.3;
     public static final double kSpeedDecrease = -0.3;
-    
+
     // PID
     public static final double kp = 1; //to be determined
     public static final double ki = 0; //to be determined
