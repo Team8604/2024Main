f35c600a8eb36c5449803e3fc5595e8255e024b7
SunayAgarwal <sagarwal@minutemanstudent.org>
2024-03-12 17:46:10 -0400

    Deadband change

--- src/main/java/frc/robot/commands/DriveRobot.java
+++ src/main/java/frc/robot/commands/DriveRobot.java
@@ -38,9 +38,9 @@ public class DriveRobot extends Command {
       /* Get forward and rotational throttle from joystick */
       /* invert the joystick Y because forward Y is negative */
       //code from joystick for drivetrain
-      fwd = -1 * MathUtil.applyDeadband(RobotContainer.m_driverController.getY(), 0.08);
-      rot1 = MathUtil.applyDeadband(RobotContainer.m_driverController.getX(), 0.08);
-      rot2 = MathUtil.applyDeadband(RobotContainer.m_driverController.getZ(), 0.08);
+      fwd = -1 * MathUtil.applyDeadband(RobotContainer.m_driverController.getY(), 0.02);
+      rot1 = MathUtil.applyDeadband(RobotContainer.m_driverController.getX(), 0.02);
+      rot2 = MathUtil.applyDeadband(RobotContainer.m_driverController.getZ(), 0.07);
 
       //gets either the bigger of twist or sideways
       rot = Math.abs(rot1) >= Math.abs(rot2) ? rot1 : rot2;
