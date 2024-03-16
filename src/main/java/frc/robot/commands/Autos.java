49744af83c23b21ebf76244d3952100a9ade0572
SunayAgarwal <sagarwal@minutemanstudent.org>
2024-03-16 16:42:11 -0400

    Update Autos.java

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -7,14 +7,20 @@ package frc.robot.commands;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public final class Autos {
+public class Autos {
   /** Example static factory for an autonomous command. */
+  private Command shootNote = new SequentialCommandGroup(null);
   public static Command exampleAuto(Drivetrain subsystem) {
     return Commands.sequence(/*subsystem.exampleMethodCommand(), */new DriveRobot());
   }
 
-  private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
+  public Autos() {
+
+  }
+
+  public Command getAuto() {
+    return null;
   }
 }
