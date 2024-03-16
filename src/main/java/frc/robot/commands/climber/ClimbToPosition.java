fdca8cf8050032ba9b5667f680e39650fc5b917d
Robert <remken07@gmail.com>
2024-03-16 10:21:22 -0400

    Create ClimbToPosition.java

---
+++ src/main/java/frc/robot/commands/climber/ClimbToPosition.java
@@ -0,0 +1,11 @@
+package frc.robot.commands.climber;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ClimbToPosition extends Command {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+    public ClimbToPosition(double desiredPosition){
+        
+    }
+}
