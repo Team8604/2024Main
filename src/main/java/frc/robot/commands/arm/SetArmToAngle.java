f1c15189d199a0b6d9e6324a830bc1cce25aeca9
Robert <remken07@gmail.com>
2024-03-18 11:53:44 -0400

    Update SetArmToAngle.java

--- src/main/java/frc/robot/commands/arm/SetArmToAngle.java
+++ src/main/java/frc/robot/commands/arm/SetArmToAngle.java
@@ -2,7 +2,6 @@ package frc.robot.commands.arm;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
